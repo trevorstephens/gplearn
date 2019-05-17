a12f92d92ad904ec8d4e54db16807c05133116c5
trevorstephens <trev.stephens@gmail.com>
2019-05-17 19:35:50 +1000

    remove unused vars

--- gplearn/tests/test_examples.py
+++ gplearn/tests/test_examples.py
@@ -23,18 +23,9 @@ from gplearn.functions import make_function
 def test_symbolic_regressor():
     """Check that SymbolicRegressor example works"""
 
-    # Ground truth
-    x0 = np.arange(-1, 1, .1)
-    x1 = np.arange(-1, 1, .1)
-    x0, x1 = np.meshgrid(x0, x1)
-
     rng = check_random_state(0)
-
-    # Training samples
     X_train = rng.uniform(-1, 1, 100).reshape(50, 2)
     y_train = X_train[:, 0] ** 2 - X_train[:, 1] ** 2 + X_train[:, 1] - 1
-
-    # Testing samples
     X_test = rng.uniform(-1, 1, 100).reshape(50, 2)
     y_test = X_test[:, 0] ** 2 - X_test[:, 1] ** 2 + X_test[:, 1] - 1
 
