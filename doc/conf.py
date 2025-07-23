98ce158fea5b87409c04a17e699380f88decea78
Colin Watson <cjwatson@debian.org>
2025-07-23 09:17:23 +0100

    Port to Sphinx 8.0 (#302)
    The old `intersphinx_mapping` format has been removed; it must now map
    identifiers to (target, inventory) tuples.

--- doc/conf.py
+++ doc/conf.py
@@ -295,4 +295,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'python': ('http://docs.python.org/', None)}
